71d73dce1949334cb20426cadb6a110b7cd918a2
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-08-08 11:10:55 -0400

    ActivityStream.feature

---
+++ src/test/java/com/bitrix24/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,19 @@
+package com.bitrix24.step_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class LoginStepDefinitions {
+
+
+    //break until 11:10
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+
+    }
+
+
+    @Given("user logs in as {string}")
+    public void user_logs_in_as(String string) {
+
+    }
+}
