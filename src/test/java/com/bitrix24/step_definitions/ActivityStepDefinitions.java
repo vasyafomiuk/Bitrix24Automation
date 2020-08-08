d2cfa4544ef1a6164db4d5a4382e385c2dc44324
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-08-08 12:37:28 -0400

    navigation method

---
+++ src/test/java/com/bitrix24/step_definitions/ActivityStepDefinitions.java
@@ -0,0 +1,9 @@
+package com.bitrix24.step_definitions;
+
+import com.bitrix24.pages.ActivityStreamPage;
+
+public class ActivityStepDefinitions {
+    ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+
+
+}
