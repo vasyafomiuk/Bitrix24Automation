2ce07b95c926d4996104a006e350a9ef090a19d4
bilaluslu <uslubilal45@gmail.com>
2020-08-08 11:44:53 -0400

    new

--- src/test/java/com/bitrix24/pages/BasePage.java
+++ src/test/java/com/bitrix24/pages/BasePage.java
@@ -14,11 +14,24 @@ public abstract class BasePage {
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
     /**
-     * Method used for navigation
+     * Method used for navigation. Module name will be specified in feature file:
+     *   And user navigates to "Activity Stream"
+     *               ||
+     *  |@Given("user navigates to {string}")
+     *  public void user_navigates_to(String module){
+     *      loginPage.navigateTo(module);
+     *  }
+     *              ||
+     *
      * @param moduleName like Tasks, Mail, Calendar, Activity Stream
      */
+
     public void navigateTo(String moduleName){
+        //  if moduleName = Activity Stream, then xpath looks like this:
+        //  span[contains(text(),'Activity Stream') and @class='menu-item-link-text']
+        //  and this xpath represents Activity Stream kink on the page
         String xpath = "//span[contains(text(),'"+moduleName+"') and @class='menu-item-link-text']";
         // wait for presence of element and click on it
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath))).click();
