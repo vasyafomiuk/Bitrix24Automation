d9c6025b8c4a63191ba21b69583ecf5f11e29496
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-08-08 11:34:42 -0400

    navigation method

--- src/test/java/com/bitrix24/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/bitrix24/step_definitions/LoginStepDefinitions.java
@@ -1,19 +1,21 @@
 package com.bitrix24.step_definitions;
 
+import com.bitrix24.pages.LoginPage;
+import com.bitrix24.utilities.ConfigurationReader;
+import com.bitrix24.utilities.Driver;
 import io.cucumber.java.en.Given;
 
 public class LoginStepDefinitions {
+    public LoginPage loginPage = new LoginPage();
 
-
-    //break until 11:10
     @Given("user is on the landing page")
     public void user_is_on_the_landing_page() {
-
+        String URL = ConfigurationReader.getProperty("url");
+        Driver.getDriver().get(URL);
     }
-
-
+    //And   user logs in as "hr"
     @Given("user logs in as {string}")
-    public void user_logs_in_as(String string) {
-
+    public void user_logs_in_as(String role) {
+        loginPage.login(role);
     }
 }
