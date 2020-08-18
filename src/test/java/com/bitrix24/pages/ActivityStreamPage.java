afeb7d9ade7dc30cc85f668d195861c52c291f59
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-08-18 10:33:07 -0400

    update

--- src/test/java/com/bitrix24/pages/ActivityStreamPage.java
+++ src/test/java/com/bitrix24/pages/ActivityStreamPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.io.File;
 
-public class ActivityStreamPage extends BasePage{
+public class ActivityStreamPage extends BasePage {
     private final static Logger logger = Logger.getLogger(ActivityStreamPage.class);
 
     @FindBy(id = "microoPostFormLHE_blogPostForm_inner")
@@ -24,24 +24,26 @@ public class ActivityStreamPage extends BasePage{
     private WebElement sendButton;
 
 
-    public void clickOnSendMessageInput(){
+    public void clickOnSendMessageInput() {
         wait.until(ExpectedConditions.visibilityOf(sendMessageInputBox)).click();
     }
 
     /**
      * File path will be provided as a parameter. Should start with src.
+     *
      * @param filePath like src/test/resources/testData/image.jpeg
      */
-    public void uploadFile(String filePath){
-        filePath = System.getProperty("user.dir")+"/"+filePath.replace("/", File.separator);
-        logger.info("File path: "+filePath);
+    public void uploadFile(String filePath) {
+        filePath = System.getProperty("user.dir") + "/" + filePath.replace("/", File.separator);
+        filePath = filePath.replace("/", File.separator);
+        logger.info("File path: " + filePath);
         wait.until(ExpectedConditions.visibilityOf(uploadFilesIcon)).click();
         BrowserUtils.waitFor(2);
         uploadFilesAndImages.sendKeys(filePath);
         BrowserUtils.waitFor(2);
     }
 
-    public void clickOnSendButton(){
+    public void clickOnSendButton() {
         wait.until(ExpectedConditions.elementToBeClickable(sendButton)).click();
         logger.info("Clicking on the Send button");
     }
